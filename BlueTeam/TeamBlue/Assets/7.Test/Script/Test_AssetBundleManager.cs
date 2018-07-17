a8805b4db18056ee47cea328e842c202ca12ca8d
김정수 <kgs1595123456@gmail.com>
2018-07-17 17:47:49 +0900

    번들 매니저

---
+++ BlueTeam/TeamBlue/Assets/7.Test/Script/Test_AssetBundleManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Test_AssetBundleManager : Singleton<Test_AssetBundleManager> {
+
+    protected Test_AssetBundleManager() { }
+    
+    public string AssetName;
+
+    AssetBundle playerBundle;
+    AssetBundle publicBundle;
+    AssetBundle nextSceneBundle;
+
+
+
+    public GameObject PlayerInstancing()
+    {
+        GameObject player;
+        player = Instantiate(playerBundle.LoadAsset(AssetName) as GameObject);
+        return player;
+    }
+
+}
