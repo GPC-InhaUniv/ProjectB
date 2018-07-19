eca294da3b89014d61b5a3ba8ec6498de3220248
이재건 <jaegun92@gmail.com>
2018-07-19 15:53:56 +0900

    MobileVersion_Commit

--- BlueTeam/TeamBlue/Assets/9.Test/Script/Test_AssetBundleManager.cs
+++ BlueTeam/TeamBlue/Assets/9.Test/Script/Test_AssetBundleManager.cs
@@ -5,65 +5,22 @@ using UnityEngine.SceneManagement;
 
 
 public class Test_AssetBundleManager : Singleton<Test_AssetBundleManager> {
-    
-    public string path;
-    protected Test_AssetBundleManager() { }
 
-    public string AssetBundleName;
+    protected Test_AssetBundleManager() { }
+    
     public string AssetName;
-    
+
     AssetBundle playerBundle;
-    
     AssetBundle publicBundle;
-    
     AssetBundle nextSceneBundle;
 
-    private void Awake()
+
+
+    public GameObject PlayerInstancing()
     {
-
-        path = System.IO.Path.Combine( Application.persistentDataPath,"/6.AssetBundles/");//"file://" + Application.dataPath + "/6.AssetBundles/character";
-    }
-    private void Start()
-    {
-        
-        StartCoroutine(PlayerBundleSeting());
-    }
-
-
-    IEnumerator PlayerBundleSeting()
-    {
-        WWW www = new WWW(path+ "character" + ".unity3d");
-            yield return www;
-            playerBundle = www.assetBundle;
-
-    }
-
-    IEnumerator PublicBundleSetting()
-    {
-        using (WWW www = new WWW(path + "public"+ ".unity3d"))
-        {
-            yield return www;
-            publicBundle = www.assetBundle;
-        }
-    }
-
-    public void LoadAsset()
-    {
-        StartCoroutine(LoadBundle());
-    }
-
-    IEnumerator LoadBundle()
-    {
-        if(nextSceneBundle != null)
-        {
-            nextSceneBundle.Unload(true);
-            nextSceneBundle = null;
-        }
-        using (WWW www = new WWW(path + AssetBundleName + ".unity3d"))
-        {
-            yield return www;
-            nextSceneBundle = www.assetBundle;   
-        }
+        GameObject player;
+        player = Instantiate(playerBundle.LoadAsset(AssetName) as GameObject);
+        return player;
     }
 
 }
