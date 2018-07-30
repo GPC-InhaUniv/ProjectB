0cd165d43cee374bbe773cd437d245458e8df3d8
김정수 <kgs1595123456@gmail.com>
2018-07-30 15:27:35 +0900

    SBT-13 TestTimer

---
+++ BlueTeam/TeamBlue/Assets/9.Test/Script/Test_Timer.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test_Timer : MonoBehaviour {
+   
+    public float deltaTime;
+
+    private void Update()
+    {
+        deltaTime = Time.deltaTime;
+    }
+}
