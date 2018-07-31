d7863382cbc478ec402eba0b8713725aac2d262a
김정수 <kgs1595123456@gmail.com>
2018-07-31 16:12:07 +0900

    SBT-62 Item Test

---
+++ BlueTeam/TeamBlue/Assets/9.Test/Script/Test_ItemCarrier.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class Test_ItemCarrier : MonoBehaviour {
+    [SerializeField]
+    public ISlotable item;
+    public Stack<ISlotable> itemSlot;
+    public Image image;
+
+
+    private void Start()
+    {
+        image = GetComponent<Image>();
+    }
+    public void SlotUpdate()
+    {
+
+    }
+
+}
