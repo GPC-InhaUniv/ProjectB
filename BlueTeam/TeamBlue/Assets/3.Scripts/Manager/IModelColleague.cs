1d9ee3d0b7e045dc2e22b17c1fe129e5419674c8
김정수 <kgs1595123456@gmail.com>
2018-07-31 14:15:35 +0900

    SBT-47 Mediator Colleague Interface 변경

--- BlueTeam/TeamBlue/Assets/3.Scripts/Manager/IModelColleague.cs
+++ BlueTeam/TeamBlue/Assets/3.Scripts/Manager/IModelColleague.cs
@@ -2,5 +2,5 @@
 
     void SendData();
     void ReceiveData();
-	
+   
 }
---
+++ BlueTeam/TeamBlue/Assets/9.Test/Script/Test_Mediator.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test_Mediator : Singleton<Test_Mediator>
+{
+    protected Test_Mediator() { }
+
+    Object Sender;
+    IModelColleague Recever;
+    
+
+   
+	
+}
