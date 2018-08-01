feb1937a7ea7f68d9d6c32deae2b23c6c959f96f
이신재 <35949895+seenjaeLee@users.noreply.github.com>
2018-08-01 19:20:39 +0900

    SBT-55 미니맵 프로토 구현 및 미니맵구현 테스트도구

---
+++ BlueTeam/TeamBlue/Assets/3.Scripts/Map/Rader.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Minimap
+{
+    public class RaderObject
+    {
+        public Image Icon { get; set; }
+        public GameObject Owner { get; set; }
+    }
+
+    public class Rader : MonoBehaviour
+    {
+        public Transform playerPos;
+        float mapScale = 2.0f;
+
+        public static List<RaderObject> raderObjects = new List<RaderObject>();
+
+        private void Update()
+        {
+            DrawRaderDots();
+        }
+        public static void RegisterRaderobject(GameObject o, Image image)
+        {
+            Image raderImage = Instantiate(image);
+            raderObjects.Add(new RaderObject() { Owner = o, Icon = raderImage });
+        }
+
+        public static void RemoveRaderObject(GameObject OwnerObject)
+        {
+            List<RaderObject> newList = new List<RaderObject>();
+            for (int i = 0; i < raderObjects.Count; i++) 
+            {
+                if(raderObjects[i].Owner == false)
+                {
+                    Destroy(raderObjects[i].Icon);
+                    continue;
+                }
+                else
+                {
+                    newList.Add(raderObjects[i]);
+                }
+                raderObjects.RemoveRange(0, raderObjects.Count);
+                raderObjects.AddRange(newList);
+            }
+        }
+
+        void DrawRaderDots()
+        {
+            foreach(RaderObject raderObject in raderObjects)
+            {
+                Vector3 raderPos = (raderObject.Owner.transform.position - playerPos.position);
+                float distanceToObject = Vector3.Distance(playerPos.position, raderObject.Owner.transform.position) * mapScale;
+                float deltay = Mathf.Atan2(raderPos.x, raderPos.z) * Mathf.Deg2Rad-270-playerPos.eulerAngles.y;
+                raderPos.x = distanceToObject * Mathf.Cos(deltay * Mathf.Deg2Rad);
+                raderPos.y = distanceToObject * Mathf.Sin(deltay * Mathf.Deg2Rad);
+
+                raderObject.Icon.transform.SetParent(this.transform);
+                raderObject.Owner.transform.position = new Vector3(raderPos.x, raderPos.z, 0) + this.transform.position;
+            }
+        }
+    }
+
+
+}
---
+++ BlueTeam/TeamBlue/Assets/9.Test/TestMinimap_seenjae/2nd/InputMouseDirection.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputMouseDirection : MonoBehaviour {
+
+    private float sensibilityX;
+
+    private void Start()
+    {
+        sensibilityX = 1.5f;
+    }
+
+    private void Update()
+    {
+        transform.Rotate(0, Input.GetAxis("Mouse X") * sensibilityX, 0);
+    }
+}
