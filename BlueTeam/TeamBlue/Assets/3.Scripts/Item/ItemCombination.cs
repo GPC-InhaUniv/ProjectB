72a5a284ce188bdc4742c429f55bb897a10546a2
이재건 <jaegun92@gmail.com>
2018-08-01 16:22:20 +0900

    SBT-65 번들 다운로드 및 클론만들기

--- BlueTeam/TeamBlue/Assets/3.Scripts/Item/ItemCombination.cs
+++ BlueTeam/TeamBlue/Assets/3.Scripts/Item/ItemCombination.cs
@@ -125,18 +125,6 @@ public class ItemCombination : MonoBehaviour
     //}
 
 
-    // Use this for initialization
-    void Start()
-    {
-
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
 
 
 }
