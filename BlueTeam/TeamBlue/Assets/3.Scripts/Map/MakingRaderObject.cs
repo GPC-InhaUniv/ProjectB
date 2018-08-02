73cc0204e39d93b07c02a81ab6603ad8691a0a06
이신재 <35949895+seenjaeLee@users.noreply.github.com>
2018-08-02 18:02:04 +0900

    SBT-55 미니맵 구현 미니맵 경로오차 수정

--- BlueTeam/TeamBlue/Assets/3.Scripts/Map/MakingRaderObject.cs
+++ BlueTeam/TeamBlue/Assets/3.Scripts/Map/MakingRaderObject.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 
 
-public class MakeRaderObject : MonoBehaviour
+public class MakingRaderObject : MonoBehaviour
 {
     public Image image;
     public Rader rader;
---
+++ BlueTeam/TeamBlue/Assets/9.Test/TestMinimap_seenjae/2nd/DungeonMinimap.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class DungeonMinimap : MonoBehaviour
+{
+    public GameObject Player;
+
+    public List<GameObject> Enemy;
+    public List<Image> EnemyDot;
+
+    private void Update()
+    {
+        
+    }
+
+    public void AddEnemyDot(GameObject enemy, Image enemyDot)
+    {
+        
+    }
+
+    
+}
