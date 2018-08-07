8ef40e328c4d4444f36beb0e037eddd195a4df05
박경태 <rudxo8812@gmail.com>
2018-08-07 17:53:16 +0900

    SBT-61 퀘스트 수정

--- BlueTeam/TeamBlue/Assets/3.Scripts/Map/Quest/BVillageQuest.cs
+++ BlueTeam/TeamBlue/Assets/3.Scripts/Map/Quest/BVillageQuest.cs
@@ -6,25 +6,22 @@ class BVillageQuest : HuntingQuest
 {
     public BVillageQuest()
     {
-        lastQuest = 0; // GameManager
+        monsterKind = Random.Range(0, 1);
         isProgress = false;
         questStateType = QuestStateType.수락;
     }
 
     public override string AcceptToQuest()
     {
-        if (lastQuest < maxQuest)
+        if (questStateType == QuestStateType.수락)
         {
-            if (questStateType == QuestStateType.수락)
-            {
-                questStateType = QuestStateType.진행;
-                isProgress = true;
-            }
-            else if (questStateType == QuestStateType.완료)
-            {
+            questStateType = QuestStateType.진행;
+            isProgress = true;
+        }
+        else if (questStateType == QuestStateType.완료)
+        {
                 questStateType = QuestStateType.수락;
                 Debug.Log("보상 : B마을 우호도 1, 경험치 500, 발전카드 1장");
-            }
         }
         else
         {
@@ -37,24 +34,16 @@ class BVillageQuest : HuntingQuest
     {
         string QuestContents = "";
         string MonsterName = "";
-        assignmentMonster = defaultMonsterCount + additionMonsterCount;
 
-        if (lastQuest < maxQuest)
+        if (monsterKind == 0)
         {
-            if (lastQuest % 2 != 1)
-            {
-                MonsterName = "돌";
-            }
-            else if (lastQuest % 2 != 0)
-            {
-                MonsterName = "철광";
-            }
-            QuestContents = "B마을 퀘스트\n" + MonsterName + " 몬스터를" + assignmentMonster + "마리 처치";
+            MonsterName = "돌";
         }
-        else
+       else if (monsterKind == 1)
         {
-            QuestContents = "B마을 퀘스트\n퀘스트 모두 완료";
+            MonsterName = "철광";
         }
+        QuestContents = "B마을 퀘스트\n" + MonsterName + " 몬스터를" + assignmentMonster + "마리 처치";
         return QuestContents;
     }
 
@@ -65,27 +54,23 @@ class BVillageQuest : HuntingQuest
 
         if (isProgress)
         {
-            if (lastQuest < maxQuest)
+            if (monsterKind == 0)
             {
-                if (lastQuest % 2 != 1)
+                if (monsterType == MonsterType.Rock)
                 {
-                    if (monsterType == MonsterType.Rock)
-                    {
-                        MonsterName = "돌";
-                        disposalMonster++;
-                    }
+                    MonsterName = "돌";
+                    disposalMonster++;
                 }
-                else
-                {
-                    if (monsterType == MonsterType.Iron)
-                    {
-                        MonsterName = "철광";
-                        disposalMonster++;
-                    }
-                }
-
-                Debug.Log(MonsterName + " 정령 한마리 처치! 남은 몬스터는 " + (assignmentMonster - disposalMonster) + "마리");
             }
+            else
+            {
+                if (monsterType == MonsterType.Iron)
+                {
+                    MonsterName = "철광";
+                    disposalMonster++;
+                }
+            }
+            Debug.Log(MonsterName + " 정령 한마리 처치! 남은 몬스터는 " + (assignmentMonster - disposalMonster) + "마리");
             CompleteToQuest();
         }
         return questStateType.ToString();
--- BlueTeam/TeamBlue/Assets/3.Scripts/Map/Quest/HuntingQuest.cs
+++ BlueTeam/TeamBlue/Assets/3.Scripts/Map/Quest/HuntingQuest.cs
@@ -38,29 +38,25 @@ interface IQuestViable
 
 abstract public class HuntingQuest : IQuestViable
 {
-    protected const int defaultMonsterCount = 30;   // 기본 몬스터 마리 수
-    protected const int maxQuest = 10;              // Quest 진행 최대 수
-    protected int additionMonsterCount;             // 퀘스트 클리어시 추가되는 몬스터 마리 수
-    protected int lastQuest;                        // DataManager에서 LastQuest를 받아야하는 데이터
-    protected int assignmentMonster;                // 잡아야할 몬스터
+    protected int monsterKind;                      // 어떤 몬스터를 잡아야하는지 결정
+    protected int assignmentMonster;           // 잡아야할 몬스터
     protected int disposalMonster;                  // 잡은 몬스터
     protected bool isProgress;                      // 퀘스트를 수락했는지 안했는지
     protected QuestStateType questStateType;        // 퀘스트 진행 상태
 
-    abstract public string AcceptToQuest();                         //퀘스트를 수락한다.
-    abstract public string ShowContentsOfQuest();                   //퀘스트 내용을 보여주다.
-    abstract public string ProceedToQuest(MonsterType monsterType); //퀘스트를 진행한다.
-    //퀘스트를 완료한다.
+    abstract public string AcceptToQuest();                         // 퀘스트를 수락한다.
+    abstract public string ShowContentsOfQuest();                   // 퀘스트 내용을 보여주다.
+    abstract public string ProceedToQuest(MonsterType monsterType); // 퀘스트를 진행한다.
+    // 퀘스트를 완료한다.
     public void CompleteToQuest()
     {
         if (assignmentMonster == disposalMonster)
         {
+            monsterKind = Random.Range(0, 1);
             isProgress = false;
             questStateType = QuestStateType.완료;
-            additionMonsterCount += 10;
             disposalMonster = 0;
             assignmentMonster = 0;
-            lastQuest++;
         }
     }
 }
