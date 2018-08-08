5c515d5cb06bbb2e34095faca3f74a1e2bf98e7b
김정수 <kgs1595123456@gmail.com>
2018-08-08 17:32:39 +0900

    폴더 수정

--- BlueTeam/TeamBlue/Assets/_1.Scripts/UI/Inventory/Inventory.cs
+++ BlueTeam/TeamBlue/Assets/_1.Scripts/UI/Inventory/Inventory.cs
@@ -6,80 +6,78 @@ using UnityEngine;
 /// </summary>
 
 
-    public enum InventoryType
+public enum InventoryType
+{
+    Equipment,
+    Resorces,
+
+}
+
+
+public class Inventory : MonoBehaviour
+{
+    public List<InventorySlot> SlotList;
+
+    private InventoryType inventoryType = InventoryType.Equipment;
+    public InventoryType InventoryType { get { return inventoryType; } }
+    void Start()
     {
-        Equipment,
-        Resorces,
+
 
     }
 
-
-    public class Inventory : MonoBehaviour
+    void LoadItem(int ItemCode, int count)
     {
 
-
-        public List<InventorySlot> SlotList;
-
-        private InventoryType inventoryType = InventoryType.Equipment;
-        public InventoryType InventoryType { get { return inventoryType; } }
-        void Start()
+    }
+    public void AddItem(Item item)
+    {
+        foreach (InventorySlot slot in SlotList)
         {
-
-
-        }
-
-        void LoadItem(int ItemCode, int count)
-        {
-
-        }
-        public void AddItem(Item item)
-        {
-            foreach (InventorySlot slot in SlotList)
+            if (!slot.isNotEmpty)
+                continue;
+            if (slot.SlotinItem.Peek().Code == item.Code)
             {
-                if (!slot.isNotEmpty)
-                    continue;
-                if (slot.SlotinItem.Peek().Code == item.Code)
-                {
-                    slot.AddItem(item);
-                    return;
-                }
-            }
-            foreach (InventorySlot slot in SlotList)
-            {
-                if (slot.isNotEmpty)
-                    continue;
-
                 slot.AddItem(item);
                 return;
-
             }
         }
-
-        public Item GetItem()
+        foreach (InventorySlot slot in SlotList)
         {
-            for (int i = 0; i < SlotList.Count; i++)
-            {
-                if (SlotList[i].isClicked)
-                    return SlotList[i].UseItem();
-            }
-            return null;
-        }
+            if (slot.isNotEmpty)
+                continue;
 
-        public void SwapOnClick(InventorySlot NewClickedSlot)
+            slot.AddItem(item);
+            return;
+
+        }
+    }
+
+    public Item GetItem()
+    {
+        for (int i = 0; i < SlotList.Count; i++)
         {
-            for (int i = 0; i < SlotList.Count; i++)
+            if (SlotList[i].isClicked)
+                return SlotList[i].UseItem();
+        }
+        return null;
+    }
+
+    public void SwapOnClick(InventorySlot NewClickedSlot)
+    {
+        for (int i = 0; i < SlotList.Count; i++)
+        {
+            if (SlotList[i].isClicked)
             {
-                if (SlotList[i].isClicked)
-                {
-                    SlotList[i].isClicked = false;
-                    NewClickedSlot.isClicked = true;
-                    return;
-                }
-                else
-                {
-                    NewClickedSlot.isClicked = true;
-                }
+                SlotList[i].isClicked = false;
+                NewClickedSlot.isClicked = true;
+                return;
+            }
+            else
+            {
+                NewClickedSlot.isClicked = true;
             }
         }
- 
+    }
+
 }
