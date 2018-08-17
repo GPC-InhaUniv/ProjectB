ff2d0af17183f85dbdb82abb70d6860f99856902
황윤우 <open1765@gmail.com>
2018-08-17 16:52:15 +0900

    SBT-18 monster State

--- BlueTeam/TeamBlue/Assets/_0.Test/Scripts/Monster/ISkillUsableBridge.cs
+++ BlueTeam/TeamBlue/Assets/_0.Test/Scripts/Monster/ISkillUsableBridge.cs
@@ -18,107 +18,85 @@ namespace ProjectB.Characters.Monsters
 
     public class NoSkill : ISkillUsableBridge
     {
-        Monster monster;
-        public Monster Monster
+        Animator animator;
+        public NoSkill(Animator animator)
         {
-            get { return monster; }
-            private set { monster = value; }
-        }
-        public NoSkill(Monster monster)
-        {
-            Monster = monster;
+            this.animator = animator;
         }
 
         public void UseSkill()
         {
-            Monster.ChangeState(Monster.State.Chasing);
+           // Monster.ChangeState(Monster.State.Chasing);
         }
 
     }
     public class NamedSkill : ISkillUsableBridge
     {
-        Monster monster;
-        public Monster Monster
-        {
-            get { return monster; }
-            private set { monster = value; }
-        }
+        Animator animator;
         GameObject skillPrefab;
 
-        public NamedSkill(Monster monster, GameObject[] skillPrefab)
+        public NamedSkill(Animator animator, GameObject[] skillPrefab)
         {
-            Monster = monster;
+            this.animator = animator;
             this.skillPrefab = skillPrefab[0];
+
         }
+
 
         public void UseSkill()
         {
-            Debug.Log(Monster.transform.position);
             //(공격,소환)스킬 오브젝트 풀에서 받아와서 사용할 예정//
 
-            skillPrefab.transform.position = Monster.transform.position;
-            skillPrefab.SetActive(true);
-
-            Monster.animator.SetBool(AniStateParm.SkillOne.ToString(),true);
+            animator.SetBool(AniStateParm.SkillOne.ToString(),true);
         }
 
     }
     public class BossSkillFirst : ISkillUsableBridge
     {
-        Boss boss;
-        public Boss Boss
+        Animator animator;
+        public BossSkillFirst(Animator animator, GameObject[] skillPrefab)
         {
-            get { return boss; }
-            private set { boss = value; }
+            this.animator = animator;
         }
-        public BossSkillFirst(Boss boss, GameObject[] skillPrefab)
-        {
 
-            Boss = boss;
-        }
         public void UseSkill()
         {
-            Boss.ChangeState(Monster.State.Chasing);
+            //Boss.ChangeState(Monster.State.Chasing);
 
             Debug.Log("boss state useskill");
         }
     }
     public class BossSkillSecond : MonoBehaviour, ISkillUsableBridge
     {
-        Boss boss;
-        public Boss Boss
-        {
-            get { return boss; }
-            private set { boss = value; }
-        }
+        Animator animator;
         GameObject SkillTest;
-        public BossSkillSecond(Boss boss, GameObject[] skillPrefab)
+
+        public BossSkillSecond(Animator animator, GameObject[] skillPrefab)
         {
-            Boss = boss;
+            this.animator = animator;
             SkillTest = skillPrefab[(int)Bossskill.SkillFireRain];
+
         }
+
         public void UseSkill()
         {
-            Boss.animator.SetBool(AniStateParm.SkillOne.ToString(), true);
+            animator.SetBool(AniStateParm.SkillOne.ToString(), true);
 
-            Instantiate(SkillTest,Boss.transform);
+           // Instantiate(SkillTest,Boss.transform);
 
             Debug.Log("boss state useskill");
         }
     }
     public class BossSkillDefence : ISkillUsableBridge
     {
-        Boss boss;
-        public Boss Boss
-        {
-            get { return boss; }
-            private set { boss = value; }
-        }
+        Animator animator;
         GameObject SkillTest;
-        public BossSkillDefence(Boss boss, GameObject[] skillPrefab)
+
+        public BossSkillDefence(Animator animator, GameObject[] skillPrefab)
         {
-            Boss = boss;
+            this.animator = animator;
             SkillTest = skillPrefab[(int)Bossskill.SkillHemiSphere];
+
         }
         public void UseSkill()
         {
@@ -129,21 +107,19 @@ namespace ProjectB.Characters.Monsters
     }
     public class BossSkillThird : MonoBehaviour, ISkillUsableBridge
     {
-        Boss boss;
-        public Boss Boss
-        {
-            get { return boss; }
-            private set { boss = value; }
-        }
+
+        Animator animator;
         GameObject SkillTest;
-        public BossSkillThird(Boss boss, GameObject[] skillPrefab)
+
+        public BossSkillThird(Animator animator, GameObject[] skillPrefab)
         {
-            Boss = boss;
+            this.animator = animator;
             SkillTest = skillPrefab[(int)Bossskill.SkillFireEntangle];
+
         }
         public void UseSkill()
         {
-            Instantiate(SkillTest, Boss.attackTarget);
+            //Instantiate(SkillTest, Boss.attackTarget);
 
             Debug.Log("boss state useskill");
         }
