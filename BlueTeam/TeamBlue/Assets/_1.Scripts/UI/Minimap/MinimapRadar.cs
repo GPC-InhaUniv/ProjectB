aafa4b190700608d51698e389f2288baf2a5d750
이신재 <magickane12@gmail.com>
2018-08-21 19:53:32 +0900

    SBT-5 미니맵 재구성

--- BlueTeam/TeamBlue/Assets/_1.Scripts/UI/Minimap/MinimapRadar.cs
+++ BlueTeam/TeamBlue/Assets/_1.Scripts/UI/Minimap/MinimapRadar.cs
@@ -3,88 +3,59 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-namespace ProjectB.Characters.Players
+namespace ProjectB.UI.Minimap
 {
     public class MinimapRadar : MonoBehaviour
     {
-        //Transform playerTransform;
-            
-        public List<GameObject> Enemys { get { return enemys; } private set { } }
-        [SerializeField]
-        List<GameObject> enemys;
+        public GameObject Player;
+        public List<GameObject> Enemys;
+        // Use this for initialization
 
-        //private void Start()
-        //{
-        //    playerTransform = GetComponentInParent<Player>().transform;
-        //}
+        //포지션 저장할 위치
+        Vector2 playerPosition;
+        Vector2 enemyPosition;
 
-        void OnTriggerEnter(Collider other)
+        public List<RectTransform> EnemyIconsPosition;
+        
+        void Update()
         {
-            if(other.tag == "Monster")
+            DrawIcons();
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (!Enemys.Contains(other.gameObject))
             {
-                if (!enemys.Contains(other.gameObject))
-                {                    
-                    enemys.Add(other.gameObject);
+                Enemys.Add(other.gameObject);
+            }
+        }
+        private void OnTriggerExit(Collider other)
+        {
+            if (Enemys.Contains(other.gameObject))
+            {
+                Enemys.Remove(other.gameObject);
+            }
+        }
+
+        
+        void DrawIcons()
+        {
+            playerPosition = new Vector2(Player.transform.position.x, Player.transform.position.z);
+
+            for (int i = 0; i < Enemys.Count; i++)
+            {
+                enemyPosition = new Vector2(Enemys[i].transform.position.x, Enemys[i].transform.position.z);
+                Vector2 playerToEnemy = enemyPosition - playerPosition;
+                EnemyIconsPosition[i].localPosition = playerToEnemy * 4.5f;
+            }
+
+            if (Enemys.Count < EnemyIconsPosition.Count)
+            {
+                for (int i = Enemys.Count; i < EnemyIconsPosition.Count; i++)
+                {
+                    EnemyIconsPosition[i].localPosition = new Vector3(100f, 0, 0);
                 }
             }
         }
-      
-        void OnTriggerExit(Collider other)
-        {
-            //Radar collider에서 사라지면 적 리스트에 들어온 적을 제거한다.
-            if (enemys.Contains(other.gameObject))
-            {
-                enemys.Remove(other.gameObject);
-            }
-        }
-
-        //플레이어 프레젠터에 있던 minimap 코드
-
-        //MinimapRadar minimap;
-
-        //[SerializeField]
-        //public List<RectTransform> enemyIconPositions;
-
-        //const float minimapScale = 5.0f;
-
-        //Vector2 playerPosition;
-        //Vector2 enemyPosition;
-
-        //public RectTransform IconsParent;
-
-        //[SerializeField]
-        //RectTransform enemyIcon;
-        //minimap
-
-        //minimap
-        //void RegistIcons()
-        //{
-        //    for (int i = 0; i < 20; i++)
-        //    {
-        //        enemyIconPositions[i] = Instantiate(enemyIcon, IconsParent.rect.position, Quaternion.identity);
-        //        enemyIconPositions[i].transform.parent = enemyIcon.parent;
-        //    }
-        //}
-
-        //void DrawIcons()
-        //{
-        //    playerPosition = new Vector2(player.transform.position.x, player.transform.position.z);
-
-        //    for (int i = 0; i < minimap.Enemys.Count; i++)
-        //    {
-        //        enemyPosition = new Vector2(minimap.Enemys[i].transform.position.x, minimap.Enemys[i].transform.position.z);
-        //        Vector2 playerToEnemy = enemyPosition - playerPosition;
-        //        enemyIconPositions[i].localPosition = playerToEnemy * minimapScale;
-        //    }
-
-        //    if (minimap.Enemys.Count < enemyIconPositions.Count)
-        //    {
-        //        for (int i = minimap.Enemys.Count; i < enemyIconPositions.Count; i++)
-        //        {
-        //            enemyIconPositions[i].localPosition = new Vector3(100f, 0, 0);
-        //        }
-        //    }
-        //}
-        //minimap
     }
 }
