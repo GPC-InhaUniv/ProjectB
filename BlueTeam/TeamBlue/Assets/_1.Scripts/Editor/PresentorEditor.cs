1b481a941e44ea09c83bad843838b80f059b63c0
박경태 <rudxo8812@gmail.com>
2018-08-29 15:44:52 +0900

    Main Canvas 수정

--- BlueTeam/TeamBlue/Assets/_1.Scripts/Editor/PresentorEditor.cs
+++ BlueTeam/TeamBlue/Assets/_1.Scripts/Editor/PresentorEditor.cs
@@ -1,63 +1,63 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEditor;
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//using UnityEditor;
 
-public class PresentorEditor
-{
+//public class PresentorEditor
+//{
 
-}
+//}
 
-[CustomEditor(typeof(EquipUIPresenter))]
-public class EquipPresentorEditor : Editor
-{
-    public override void OnInspectorGUI()
-    {
+//[CustomEditor(typeof(EquipUIPresenter))]
+//public class EquipPresentorEditor : Editor
+//{
+//    public override void OnInspectorGUI()
+//    {
 
-    }
-}
+//    }
+//}
 
-[CustomEditor(typeof(CombinationUIPresenter))]
-public class CombinationPresenterEditor : Editor
-{
-    public override void OnInspectorGUI()
-    {
+//[CustomEditor(typeof(CombinationUIPresenter))]
+//public class CombinationPresenterEditor : Editor
+//{
+//    public override void OnInspectorGUI()
+//    {
 
-    }
-}
+//    }
+//}
 
-[CustomEditor(typeof(QuestUIPresenter))]
-public class QuestPresenter : Editor
-{
-    public override void OnInspectorGUI()
-    {
+//[CustomEditor(typeof(QuestUIPresenter))]
+//public class QuestPresenter : Editor
+//{
+//    public override void OnInspectorGUI()
+//    {
 
-    }
-}
+//    }
+//}
 
-[CustomEditor(typeof(InventoryUIPresenter))]
-public class InventoryPresenter : Editor
-{
-    public override void OnInspectorGUI()
-    {
+//[CustomEditor(typeof(InventoryUIPresenter))]
+//public class InventoryPresenter : Editor
+//{
+//    public override void OnInspectorGUI()
+//    {
 
-    }
-}
+//    }
+//}
 
-[CustomEditor(typeof(WarehouseUIPresenter))]
-public class WarehousePresenter : Editor
-{
-    public override void OnInspectorGUI()
-    {
+//[CustomEditor(typeof(WarehouseUIPresenter))]
+//public class WarehousePresenter : Editor
+//{
+//    public override void OnInspectorGUI()
+//    {
 
-    }
-}
+//    }
+//}
 
-[CustomEditor(typeof(WorldMapUIPresenter))]
-public class WorldMapUIyPresenter : Editor
-{
-    public override void OnInspectorGUI()
-    {
+//[CustomEditor(typeof(WorldMapUIPresenter))]
+//public class WorldMapUIyPresenter : Editor
+//{
+//    public override void OnInspectorGUI()
+//    {
 
-    }
-}
+//    }
+//}
