f12021927fe9595e9c493d33439e205c7443ecfd
황윤우 <open1765@gmail.com>
2018-08-29 17:47:02 +0900

    SBT-82 Folder정리

--- BlueTeam/TeamBlue/Assets/_1.Scripts/Character/Player/Character.cs
+++ BlueTeam/TeamBlue/Assets/_1.Scripts/Character/Player/Character.cs
@@ -5,21 +5,22 @@ namespace ProjectB.Characters
 {
     public abstract class Character : MonoBehaviour
     {
+        [SerializeField]
         protected float healthPoint;
         public float HealthPoint { get { return healthPoint; } private set { } }
-
+        [SerializeField]
         protected float maxHealthPoint;
         public float MaxHealthPoint { get { return maxHealthPoint; } private set { } }
-
+        [SerializeField]
         protected int level;
         public int Level { get { return level; } private set { } }
-
+        [SerializeField]
         protected float exp;
         public float Exp { get { return exp; } private set { } }
-
+        [SerializeField]
         protected float attackPower;
         public float AttackPower { get { return attackPower; } private set { } }
-
+        [SerializeField]
         protected float defensivePower;
 
         public abstract void ReceiveDamage(float damage);
