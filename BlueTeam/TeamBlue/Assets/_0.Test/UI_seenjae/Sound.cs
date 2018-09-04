0efccf792bea4d3811fb5a17768dd574e4350b59
이신재 <magickane12@gmail.com>
2018-09-04 17:43:06 +0900

    no message

---
+++ BlueTeam/TeamBlue/Assets/_0.Test/UI_seenjae/Sound.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class Sound : MonoBehaviour
+{
+    public AudioClip SoundClip;
+
+    [Range(0.0f,1.0f)]
+    public float Volume;
+    
+
+    public void RegistSoundClip()
+    {
+
+    }
+
+    public void ColtrolVolume()
+    {
+
+    }
+	
+}
